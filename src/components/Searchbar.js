97509947c033eb7bba176121d021d9a302cbf988
emrethebuffguy <95050288+emrethebuffguy@users.noreply.github.com>
2022-07-30 00:18:05 +0300

    Searchbar remove button commit
    delete individual features from search bar feature added

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,18 +1,37 @@
-const Searchbar = ({tagsList,setTagsList})=>{
-    return (
-      <div className="search-bar">
-        <div className="search-container">
-          {tagsList.map((item, index) => {
-            return (
-              <button key={index} className="tag-btn">
-                {item}
-              </button>
-            );
-          })}
-        </div>
-        <button className="clear-btn" onClick={()=>{setTagsList([])}}>clear</button>
+const Searchbar = ({ tagsList, setTagsList }) => {
+  const handleClick = (name) => {
+    let newArray = tagsList.filter((element) => {
+      return element !== name;
+    });
+    setTagsList(newArray);
+  };
+  return (
+    <div className="search-bar">
+      <div className="search-container">
+        {tagsList.map((item, index) => {
+          return (
+            <button
+              key={index}
+              className="tag-btn"
+              onClick={() => {
+                handleClick(item);
+              }}
+            >
+              {item}
+            </button>
+          );
+        })}
       </div>
-    );
-}
+      <button
+        className="clear-btn"
+        onClick={() => {
+          setTagsList([]);
+        }}
+      >
+        clear
+      </button>
+    </div>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
